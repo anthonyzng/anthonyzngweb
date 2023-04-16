aa052ea9e3a275120090cb90eadf182d275c0fd0
AnthonyzNg <71817597+anthonyzng@users.noreply.github.com>
2023-04-15 23:52:22 -0400

    init-version-myweb

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// created component
+import { HomeComponent } from './home/home.component';
+import { CvComponent } from './cv/cv.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProjectComponent } from './project/project.component';
+import { ToolsComponent } from './tools/tools.component';
+import { FooterComponent } from './footer/footer.component';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'cv', component: CvComponent},
+  {path: 'project', component: ProjectComponent},
+  {path: 'tool', component: ToolsComponent},
+  {path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'my-icon',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('/assets/c_linkg_pig.svg')
+    );
+    }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//main core component
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// created component
+import { HomeComponent } from './home/home.component';
+import { CvComponent } from './cv/cv.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProjectComponent } from './project/project.component';
+import { ToolsComponent } from './tools/tools.component';
+import { FooterComponent } from './footer/footer.component';
+//angular material 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CvComponent,
+    NotFoundComponent,
+    ProjectComponent,
+    ToolsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
