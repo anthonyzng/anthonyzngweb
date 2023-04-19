13a7d62dd479bf91f7e5e10f41559465fe408884
AnthonyzNg <71817597+anthonyzng@users.noreply.github.com>
2023-04-19 03:29:00 -0400

    complete info component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // created component
 import { HomeComponent } from './home/home.component';
-import { CvComponent } from './cv/cv.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ProjectComponent } from './project/project.component';
 import { ToolsComponent } from './tools/tools.component';
@@ -10,10 +9,11 @@ import { FooterComponent } from './footer/footer.component';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { GameComponent } from './game/game.component';
+import { AboutComponent } from './about/about.component'; 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'cv', component: CvComponent},
+  {path: 'about', component: AboutComponent},
   {path: 'project', component: ProjectComponent},
   {path: 'tool', component: ToolsComponent},
   {path: 'game', component: GameComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // created component
 import { HomeComponent } from './home/home.component';
-import { CvComponent } from './cv/cv.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ProjectComponent } from './project/project.component';
 import { ToolsComponent } from './tools/tools.component';
 import { FooterComponent } from './footer/footer.component';
 import { GameComponent } from './game/game.component';
+import { BackgroundComponent } from './background/background.component';
 //angular material 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -20,19 +20,22 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { HttpClientModule } from '@angular/common/http';
 import { MatMenuModule } from '@angular/material/menu';
-import { BackgroundComponent } from './background/background.component';
-import {MatCardModule} from '@angular/material/card'; 
+import { MatCardModule } from '@angular/material/card';
+import {LayoutModule} from '@angular/cdk/layout';
+import { InfoComponent } from './info/info.component'; 
+
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CvComponent,
     NotFoundComponent,
     ProjectComponent,
     ToolsComponent,
     FooterComponent,
     GameComponent,
-    BackgroundComponent
+    BackgroundComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
@@ -45,7 +48,8 @@ import {MatCardModule} from '@angular/material/card';
     MatListModule,
     HttpClientModule,
     MatMenuModule,
-    MatCardModule
+    MatCardModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
