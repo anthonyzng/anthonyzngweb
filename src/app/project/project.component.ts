96569598820bbfd8cf7a5509555bdcd85f76184e
AnthonyzNg <71817597+anthonyzng@users.noreply.github.com>
2023-05-01 02:36:58 -0400

    build test for version1_3

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -10,7 +10,7 @@ export class ProjectComponent {
   cardData:CardData[] = [
     {title:"Book System",img:"./assets/book_fullstack.png",demo_link:"https://drive.google.com/file/d/1ZZyOJdGPsz08wsMyhBZlMuWr0ZQNkSYv/view",code_link:"",tech:"Tech-stack : AngularCLI + MySQL + NodeJs , Express(Rest-API) , MongoDb , ELM"},
     {title:"Online TODO List Mobile application",img:"./assets/todo.png",demo_link:"https://drive.google.com/file/d/1K3Gis8FFDylVvMQryFOznIFraseatQqr/view",code_link:"",tech:"Tech-stack : Android + NodeJs + Express"},
-    {title:"Online TODO List Mobile application",img:"./assets/dog.png",demo_link:"https://drive.google.com/file/d/1gXKdiSSjUt9m6SMwhnqzV95KUGzHd3mp/view",code_link:"",tech:"Tech-stack : Android + TensorFlowLite + Python ,This application aims to compare RCNN & CNN model in computer vision area."}
+    {title:"Comparing Computer Vision Models for Dog Breed Identification",img:"./assets/dog.png",demo_link:"https://drive.google.com/file/d/1gXKdiSSjUt9m6SMwhnqzV95KUGzHd3mp/view",code_link:"",tech:"Tech-stack : Android + TensorFlowLite + Python ,This application aims to compare RCNN & CNN model in computer vision area."}
   ]
   openUrl(url : string){
     window.open(url);
